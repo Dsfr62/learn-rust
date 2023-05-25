b458cc9a8d849a7e966a6792be83e95fab10409d
Dsfr62 <saviodudu79@gmail.com>
2023-05-25 16:18:21 -0300

    second commit

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,14 @@ fn main() {
     println!("First: {sec_variable}");
     const VARIABLE_CONSTANTE: &str = "variavel constante";
     println!("{VARIABLE_CONSTANTE}");
+    let numero: u8 = 3;
+    let sec_numero = 2023;
+    println!("variavel: {}, tamanho = {} bytes", numero, std::mem::size_of_val(&numero));
+    println!("variavel: {}, tamanho = {} bytes", sec_numero, std::mem::size_of_val(&sec_numero));
+
+    let floaters: f32 = 2.5;
+    println!("variavel: {}, tamanho = {} bytes", floaters, std::mem::size_of_val(&floaters));
+
+    let verdadeiro: bool = true;
+    println!("{verdadeiro}");
 }
